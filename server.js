6a11f286557c410b3123058cc904f6361c8cca6d
steven-mpawulo <stevenmpawulo@gmail.com>
2023-09-08 11:13:09 +0300

    adds file url and repo url

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.get('/api', (req, res) => {
         "current_day": dayOfTheWeek,
         "utc_time": date,
         "track": track,
-        "github_file_url": "https://github.com/username/repo/blob/main/file_name.ext",
-        "github_repo_url": "https://github.com/username/repo",
+        "github_file_url": "https://github.com/steven-mpawulo/hng-assignment-one/blob/main/server.js",
+        "github_repo_url": "https://github.com/steven-mpawulo/hng-assignment-one",
         "status_code": 200
       });
 
